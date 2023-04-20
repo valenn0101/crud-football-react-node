97c16d91e85211947ec354271d90bf9835d8b2d8
valenn0101 <veroncaceresvalentin@gmail.com>
2023-04-20 13:25:31 -0300

    El año del footer se actualizara automáticamente

--- frontend-crud/src/components/Footer/Footer.jsx
+++ frontend-crud/src/components/Footer/Footer.jsx
@@ -1,11 +1,13 @@
-import React from 'react'
-import styles from './Footer.module.css'
-
+import React from "react";
+import styles from "./Footer.module.css";
 
 export default function Footer() {
-    return (
-      <footer className={styles.footer}>
-        <p>&copy; 2023 Premier League Teams by <b>Valentin Veron Caceres</b></p>
-      </footer>
-    );
-  }
+  return (
+    <footer className={styles.pageFooter}>
+      <p>
+        &copy; ${new Date().getFullYear()} Premier League Teams by{" "}
+        <b>Valentin Veron Caceres</b>
+      </p>
+    </footer>
+  );
+}
